3125707c855ac185ad185ffe8b2f6f8961af12f6
doha-kim <doha-kim@nhn-commerce.com>
2025-07-19 01:56:06 +0900

    feat: OAS(OpenAPI Specification) 구축

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.kotlin.dsl.support.isGradleKotlinDslJar
+
 plugins {
 	kotlin("jvm") version "2.1.0"
 	kotlin("kapt") version "2.1.0"
@@ -5,8 +7,10 @@ plugins {
 	kotlin("plugin.jpa") version "2.1.0"
 	id("org.springframework.boot") version "3.4.1"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("com.epages.restdocs-api-spec") version "0.19.4"
 }
 
+val restdocsApiSpecVersion: String by extra { "0.19.4" }
 fun getGitHash(): String {
 	return providers.exec {
 		commandLine("git", "rev-parse", "--short", "HEAD")
@@ -58,9 +62,34 @@ dependencies {
 	testImplementation("org.testcontainers:junit-jupiter")
 	testImplementation("org.testcontainers:mysql")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	// REST Docs
+	testImplementation("com.epages:restdocs-api-spec-mockmvc:${restdocsApiSpecVersion}")
+	testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
 }
 
 tasks.withType<Test> {
 	useJUnitPlatform()
 	systemProperty("user.timezone", "UTC")
+}
+
+openapi3 {
+	setServer("https://localhost:8080")
+	title = "Concert-System API"
+	description = """
+                  | Concert-System API 명세서입니다.
+                  """.trimMargin()
+	version = "1.0.0"
+	format = "yaml"
+}
+
+tasks.register<Copy>("copyOpenApiSpec") {
+	delete("${layout.projectDirectory}/src/main/resources/static/swagger-ui/openapi3.yaml")
+	from("${layout.buildDirectory}/api-spec/openapi3.yaml")
+	into("${layout.projectDirectory}/src/main/resources/static/swagger-ui/.")
+	onlyIf { file("${layout.buildDirectory}/api-spec/openapi3.yaml").exists() }
+}
+
+tasks.named("build") {
+	finalizedBy("copyOpenApiSpec")
 }
